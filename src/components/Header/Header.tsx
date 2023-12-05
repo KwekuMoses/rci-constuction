2fee1af573bd96cbfbfc5efe66241c7e9973473e
kweku <kweku.moses@hotmail.com>
2023-12-05 12:08:34 +0100

    Check screen size in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,25 @@
+'use client';
+
 import React from 'react';
 import './Header.scss';
 import Navigation from '../Navigation/Navigation';
 import Button from '../Button/Button';
 import Logo from '../Logo/Logo';
 
+import { useWindowSize } from '@/Hooks/useWindowSize';
+import { isMobileScreen } from '@/Utils/handleResponsive';
+
 const Header = () => {
+    const { width } = useWindowSize();
+    const isMobile = isMobileScreen(width);
 
 
     return (
         <div className="Header" id="Header">
+
             <div className="Header__Column"><Logo /></div>
-            <div className="Header__Column"><Navigation /></div>
-            <div className="Header__Column"><Button /></div>
+            <div className="Header__Column"><Navigation isMobile={isMobile} /></div>
+            {!isMobile && <div className="Header__Column"><Button /></div>}
         </div>
     );
 };
