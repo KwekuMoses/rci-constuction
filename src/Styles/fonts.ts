bf102d0b7a3dc6d0fee509e276e0f2b731a43331
kweku <kweku.moses@hotmail.com>
2023-12-05 15:25:06 +0100

    Add DM_Sans font and update CtaButton style

--- src/Styles/fonts.ts
+++ src/Styles/fonts.ts
@@ -1,7 +1,13 @@
 
 import { Poppins } from 'next/font/google'
+import { DM_Sans } from 'next/font/google'
 
 export const poppins = Poppins({
     weight: '700',
+    subsets: ['latin'],
+})
+
+export const dm_sans = DM_Sans({
+    weight: '500',
     subsets: ['latin'],
 })
