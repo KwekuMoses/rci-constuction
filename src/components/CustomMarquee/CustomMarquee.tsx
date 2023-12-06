1d39ac1f3caa260ab95f1293b5c376714ae16cf8
kweku <kweku.moses@hotmail.com>
2023-12-06 15:38:39 +0100

    Add CustomMarquee component with styles and images

--- src/components/CustomMarquee/CustomMarquee.tsx
+++ src/components/CustomMarquee/CustomMarquee.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import './CustomMarquee.scss';
+import Image from 'next/image';
+import Marquee from "react-fast-marquee";
 
 interface Props {
     logoData: {
@@ -9,11 +12,20 @@ interface Props {
 }
 
 const CustomMarquee = ({ logoData }: Props) => {
-    const { image_1, image_2, image_3 } = logoData
+    const { image_1, image_2, image_3 } = logoData;
 
     return (
-        <div>CustomMarquee</div>
-    )
+        <div className="Marquee">
+            <div className="Marquee__Content">
+                <Marquee>
+                    {image_1 && <div className="Marquee__ImageContainer--first"><Image src={image_1} alt="Logo 1" layout="responsive" width={100} height={50} /></div>} {/* Adjust width and height as needed */}
+                    {image_2 && <div className="Marquee__ImageContainer--second"><Image src={image_2} alt="Logo 2" layout="responsive" width={100} height={50} /></div>}
+                    {image_3 && <div className="Marquee__ImageContainer"><Image src={image_3} alt="Logo 3" layout="responsive" width={100} height={50} /></div>}
+                </Marquee>
+            </div>
+        </div>
+
+    );
 }
 
-export default CustomMarquee
+export default CustomMarquee;
