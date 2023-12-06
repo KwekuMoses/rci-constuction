bdc170e245e140068ac508c6a3162a2d72c6dc22
kweku <kweku.moses@hotmail.com>
2023-12-06 16:32:19 +0100

    Update image priority in CustomMarquee component

--- src/components/CustomMarquee/CustomMarquee.tsx
+++ src/components/CustomMarquee/CustomMarquee.tsx
@@ -18,9 +18,9 @@ const CustomMarquee = ({ logoData }: Props) => {
         <div className="Marquee">
             <div className="Marquee__Content">
                 <Marquee>
-                    {image_1 && <div className="Marquee__ImageContainer--first"><Image src={image_1} alt="Logo 1" layout="responsive" width={100} height={50} /></div>} {/* Adjust width and height as needed */}
-                    {image_2 && <div className="Marquee__ImageContainer--second"><Image src={image_2} alt="Logo 2" layout="responsive" width={100} height={50} /></div>}
-                    {image_3 && <div className="Marquee__ImageContainer"><Image src={image_3} alt="Logo 3" layout="responsive" width={100} height={50} /></div>}
+                    {image_1 && <div className="Marquee__ImageContainer--first"><Image src={image_1} alt="Logo 1" layout="responsive" width={100} height={50} priority={true} /></div>}
+                    {image_2 && <div className="Marquee__ImageContainer--second"><Image src={image_2} alt="Logo 2" layout="responsive" width={100} height={50} priority={true} /></div>}
+                    {image_3 && <div className="Marquee__ImageContainer"><Image src={image_3} alt="Logo 3" layout="responsive" width={100} height={50} priority={true} /></div>}
                 </Marquee>
             </div>
         </div>
