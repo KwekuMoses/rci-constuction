7a6d1a01b71ce306cbe91f3dd082dbab6e030872
kweku <kweku.moses@hotmail.com>
2023-12-07 18:47:34 +0100

    Add ContactSection component with styles

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import CtaButton from '../CtaButton/CtaButton'
+import './ContactSection.scss'
 
 const ContactSection = () => {
     return (
-        <div>ContactSection</div>
+        <div className="ContactSection">
+            <h3 className="ContactSection__Title">Kontakta oss idag</h3>
+            <p className="ContactSection__Text">Låt oss Bygga något bra tillsammans</p>
+            <CtaButton link={"/"} width="medium" />
+        </div>
     )
 }
 
