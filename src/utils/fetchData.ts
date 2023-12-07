4e088a8be112fe014938ebc2dc76bab5d4b0ac3c
kweku <kweku.moses@hotmail.com>
2023-12-07 22:14:22 +0100

    Refactor fetchProjectsData to handle errors and include project ID in the URL

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -53,13 +53,19 @@ export const fetchServicesData = cache(async (id: number) => {
     return res.json()
 })
 
-export const fetchProjectsData = cache(async () => {
-    const res = await fetch(`${BASE_URL}/project`)
 
-    if (!res.ok) {
-        throw new Error('Failed to fetch data')
+export const fetchProjectsData = cache(async (id: number | string) => {
+    try {
+        const res = await fetch(`${BASE_URL}/project/${id}`);
+
+        if (!res.ok) {
+            throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
+        }
+
+        return res.json();
+    } catch (error) {
+        console.error("Error fetching project data:", error);
+        // You can also rethrow the error if you want to handle it further up the call stack
+        // throw error;
     }
-
-    return res.json()
-})
-
+});
