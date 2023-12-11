a6360599965528d33671375e6e1b083665b4fc2a
kweku <kweku.moses@hotmail.com>
2023-12-11 15:20:34 +0100

    Fix aspect-ratio lighthouse issue in DesktopMarquee

--- src/components/DesktopMarquee/DesktopMarquee.tsx
+++ src/components/DesktopMarquee/DesktopMarquee.tsx
@@ -19,9 +19,9 @@ const DesktopMarquee = ({ logoData }: Props) => {
         <div className="DesktopMarquee">
             <Title title="Vi sammarbetar med" />
             <div className="DesktopMarquee__Content">
-                {image_1 && <div className="DesktopMarquee__ImageContainer DesktopMarquee__ImageContainer--first"><Image src={image_1} alt="Logo 1" layout="responsive" width={100} height={50} priority={true} /></div>}
-                {image_2 && <div className="DesktopMarquee__ImageContainer DesktopMarquee__ImageContainer--second"><Image src={image_2} alt="Logo 2" layout="responsive" width={100} height={50} priority={true} /></div>}
-                {image_3 && <div className="DesktopMarquee__ImageContainer"><Image src={image_3} alt="Logo 3" layout="responsive" width={100} height={50} priority={true} /></div>}
+                {image_1 && <div className="DesktopMarquee__ImageContainer DesktopMarquee__ImageContainer--first"><Image src={image_1} alt="Logo 1" layout="responsive" width={390} height={127} priority={true} /></div>}
+                {image_2 && <div className="DesktopMarquee__ImageContainer DesktopMarquee__ImageContainer--second"><Image src={image_2} alt="Logo 2" layout="responsive" width={524} height={96} priority={true} /></div>}
+                {image_3 && <div className="DesktopMarquee__ImageContainer"><Image src={image_3} alt="Logo 3" layout="responsive" width={368} height={469} priority={true} /></div>}
             </div>
         </div>
 
