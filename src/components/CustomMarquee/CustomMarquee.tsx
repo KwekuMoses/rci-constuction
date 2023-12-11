5db5bbc5fa0af3a497cc5308bcd01c7f98a45e7a
kweku <kweku.moses@hotmail.com>
2023-12-11 14:39:30 +0100

    Hard code aspect ratios in Custom Marquee

--- src/components/CustomMarquee/CustomMarquee.tsx
+++ src/components/CustomMarquee/CustomMarquee.tsx
@@ -22,9 +22,35 @@ const CustomMarquee = ({ logoData }: Props) => {
             <Title title="Vi sammarbetar med" />
             <div className="Marquee__Content">
                 <Marquee>
-                    {image_1 && <div className="Marquee__ImageContainer Marquee__ImageContainer--first"><Image src={image_1} alt="Logo 1" layout="responsive" width={100} height={50} priority={true} /></div>}
-                    {image_2 && <div className="Marquee__ImageContainer Marquee__ImageContainer--second"><Image src={image_2} alt="Logo 2" layout="responsive" width={100} height={50} priority={true} /></div>}
-                    {image_3 && <div className="Marquee__ImageContainer"><Image src={image_3} alt="Logo 3" layout="responsive" width={100} height={50} priority={true} /></div>}
+                    {image_1 &&
+                        <div className="Marquee__ImageContainer Marquee__ImageContainer--first">
+                            <Image
+                                src={image_1}
+                                alt="Logo 1"
+                                layout="responsive"
+                                width={390}
+                                height={127}
+                                priority={true} />
+                        </div>}
+                    {image_2 &&
+                        <div className="Marquee__ImageContainer Marquee__ImageContainer--second">
+                            <Image
+                                src={image_2}
+                                alt="Logo 2"
+                                layout="responsive"
+                                width={524}
+                                height={96}
+                                priority={true} />
+                        </div>}
+                    {image_3 &&
+                        <div className="Marquee__ImageContainer">
+                            <Image
+                                src={image_3}
+                                alt="Logo 3"
+                                layout="responsive"
+                                width={368}
+                                height={469}
+                                priority={true} /></div>}
                 </Marquee>
             </div>
         </div>
