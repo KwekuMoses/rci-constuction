fa5498b56fba63008bc83e9443393b8190376b14
kweku <kweku.moses@hotmail.com>
2023-12-11 17:51:19 +0100

    Add NotFound component and styles

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './not-found.scss'
+
+const NotFound = () => {
+    return (
+        <div className="NotFound"><b>404</b>Denna sida existerar inte</div>
+    )
+}
+
+export default NotFound
