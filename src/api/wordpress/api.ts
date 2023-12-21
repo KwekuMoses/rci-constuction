faede54817c27c0c5e1214cda9f8c4b7f6038aec
kweku <kweku.moses@hotmail.com>
2023-12-21 18:03:17 +0100

    Refactor fetches for heroData and navigationData to use GraphQL

--- src/api/wordpress/api.ts
+++ src/api/wordpress/api.ts
@@ -1,12 +1,70 @@
-"use server"
+// "use server"
 import 'server-only'
 import { cache } from 'react'
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+const URL = process.env.NEXT_PUBLIC_URL
+const PASSWORD = process.env.NEXT_PUBLIC_LOCAL_PASSWORD
+const USERNAME = process.env.NEXT_PUBLIC_USERNAME
+// Encode your username and password in base64
+const encodedCredentials = Buffer.from(`${USERNAME}:${PASSWORD}`).toString('base64');
 
 export const preloadHomeData = (id: number) => {
     void fetchPageData(id)
 }
+
+export const fetchGraphQLData = async (query: string) => {
+    const res = await fetch(`${URL}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Basic ${encodedCredentials}`
+        },
+        body: JSON.stringify({ query }),
+    });
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data');
+    }
+
+    return res.json();
+};
+
+export const fetchHeroData = cache(async (uri: string) => {
+    const query = `
+    {
+        pageBy(uri: "${uri}") {
+          home {
+            hero {
+              fieldGroupName
+              subtitle
+              tagline
+              title
+              imageurl {
+                sourceUrl
+              }
+            }
+          }
+        }
+    }`;
+
+    return fetchGraphQLData(query);
+});
+
+export const fetchNavigationData = cache(async () => {
+    const query = `
+    {
+        pages {
+            nodes {
+              title
+              slug
+              menuOrder
+            }
+          }
+      }`;
+
+    return fetchGraphQLData(query);
+});
 
 export const fetchPageData = cache(async (id: number) => {
     const res = await fetch(`${BASE_URL}/pages/${id}`)
@@ -33,15 +91,15 @@ export const fetchServiceData = cache(async () => {
 
 })
 
-export const fetchNavigationData = cache(async () => {
-    const res = await fetch(`${BASE_URL}/navigation`)
+// export const fetchNavigationData = cache(async () => {
+//     const res = await fetch(`${BASE_URL}/navigation`)
 
-    if (!res.ok) {
-        throw new Error('Failed to fetch data')
-    }
+//     if (!res.ok) {
+//         throw new Error('Failed to fetch data')
+//     }
 
-    return res.json()
-})
+//     return res.json()
+// })
 
 export const fetchServicesData = cache(async (id: number) => {
     const res = await fetch(`${BASE_URL}/service/${id}`)
