c5c3673479b7629b62a9c9d585c6276d23a6f01d
kweku <kweku.moses@hotmail.com>
2023-12-21 18:03:41 +0100

    Refactor NavigationLinks component

--- src/components/navigation-links/NavigationLinks.tsx
+++ src/components/navigation-links/NavigationLinks.tsx
@@ -1,87 +1,50 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import './NavigationLinks.scss';
 import { isActiveLink } from '@/utils/routeHelpers';
-import { parseNavigationHTML } from '@/utils/parseNavigationHTML';
-import { fetchNavigationData } from '@/api/wordpress/api';
+
+interface NavigationItem {
+    title: string;
+    slug: string;
+    menuOrder: number;
+}
+
 interface Props {
     isMobile: boolean;
-    servicesData: {};
-    navigationData: [{
-        content: {
-            rendered: string
-        }
-    }] | {};
+    servicesData: {}; 
+    navigationData: NavigationItem[];
     setIsOpen: (isOpen: boolean) => void;
 }
 
 const NavigationLinks = ({ isMobile, servicesData, navigationData, setIsOpen }: Props) => {
-    const [data, setData] = React.useState([]);
 
     const pathname = usePathname();
 
-    const navigationDataString = Array.isArray(navigationData) && navigationData[0]?.content?.rendered
-        ? navigationData[0].content.rendered
-        : '';
+    const sortedNavigationData = [...navigationData].sort((a, b) => a.menuOrder - b.menuOrder);
 
-    const links = navigationDataString
-        ? parseNavigationHTML(navigationDataString)
-        : [];
-
-
-    useEffect(() => {
-        const fetchData = async () => {
-            let data = await fetchNavigationData()
-            setData(data)
+    const Links = sortedNavigationData.map((node) => {
+        let link = `/${node.slug}`;
+        if (node.slug === 'tjanster') {
+            link = '/#tjanster';
         }
-        fetchData()
-    }
-        , [])
+        if (node.slug === 'hem') {
+            link = '/';
+        }
 
-    console.log(data)
-
-    const Links = [
-        {
-            page: 'Hem',
-            link: '/',
-            id: 1
-        },
-        {
-            page: 'Tjänster',
-            link: '/#tjanster',
-            id: 2
-        },
-        {
-            page: 'Våra Projekt',
-            link: '/vara-projekt',
-            id: 3
-        },
-        {
-            page: 'Karriär',
-            link: '/karriar',
-            id: 4
-        },
-
-        {
-            page: 'Om oss',
-            link: '/om-oss',
-            id: 5
-        },
-
-        {
-            page: 'Kontakt',
-            link: '/kontakt',
-            id: 6
-        },
-    ]
+        return {
+            page: node.title,
+            link,
+            id: node.slug
+        };
+    });
 
     return (
         <div className={`NavigationLinks NavigationLinks--${isMobile ? "Mobile" : "Desktop"}`}>
             {Links.map(({ page, link, id }) => (
                 <Link
                     className={`NavigationLinks__Link ${isActiveLink(pathname, link)}`}
-                    href={`${link}`}
+                    href={link}
                     key={id}
                     onClick={() => setIsOpen(false)}
                 >
